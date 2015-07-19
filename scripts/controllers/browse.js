55243f8fddaa5244e29cb4a5d3c9df197c346855
Raj Bhanushali <raj.bshali@gmail.com>
2015-07-19 07:16:08 -0700

    more responsive - added media queries

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -3,9 +3,34 @@
 app.controller("BrowseController", function($scope, uiGmapGoogleMapApi){
 
 	$scope.map = {
-	center: { latitude: 45, longitude: -73 },
-	zoom: 8 };
-	uiGmapGoogleMapApi.then(function(maps){
-	});
+	center: {
+	latitude: 37.763912,
+	longitude: -121.8899834},
+	zoom: 18 };
+
+	$scope.marker = {
+		id: 0,
+		coords: {
+			latitude: 37.763912,
+			longitude: -121.8899834
+		},
+		show:false
+	};
+
+
+        $scope.windowOptions = {
+            visible: false
+        };
+
+        $scope.onClick = function() {
+            $scope.windowOptions.visible = !$scope.windowOptions.visible;
+        };
+
+        $scope.closeClick = function() {
+            $scope.windowOptions.visible = false;
+        };
+
+        $scope.title = "Window Title!";
+	
 
 });
